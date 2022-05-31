1f9a8b7dd6f2c5cacce1083008804a62dadf5b8c
ankita2503 <37950041+ankita2503@users.noreply.github.com>
2022-05-31 21:23:23 +0200

    Update GenerateRandomStringBounded.java

--- GenerateRandomStringBounded.java
+++ GenerateRandomStringBounded.java
@@ -1,13 +1,17 @@
-public static void main(String...args){
-    int leftLimit=97; // letter 'a'
-    int rightLimit=122; // letter 'z'
-    int targetStringLength=10;
-    Random random=new Random();
-    StringBuilder buffer=new StringBuilder(targetStringLength);
-    for(int i=0;i<targetStringLength; i++){
-        int randomLimitedInt=leftLimit+(int)(random.nextFloat()*(rightLimit-leftLimit+1));
-        buffer.append((char)randomLimitedInt);
-}
-    String generatedString=buffer.toString();
-    System.out.println(generatedString);
+import java.util.Random;
+
+public class GenerateRandomStringBounded {
+    public static void main(String...args){
+        int leftLimit=97; // letter 'a'
+        int rightLimit=122; // letter 'z'
+        int targetStringLength=10;
+        Random random=new Random();
+        StringBuilder buffer=new StringBuilder(targetStringLength);
+        for(int i=0;i<targetStringLength; i++){
+            int randomLimitedInt=leftLimit+(int)(random.nextFloat()*(rightLimit-leftLimit+1));
+            buffer.append((char)randomLimitedInt);
+        }
+        String generatedString=buffer.toString();
+        System.out.println(generatedString);
+    }
 }
